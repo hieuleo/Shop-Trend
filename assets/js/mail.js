8179e5c828bf10663a1ac0982e3994436aceb587
Nguyễn Quốc Hiếu <83700130+hieuleo@users.noreply.github.com>
2021-09-10 00:24:43 +0700

    first commit

---
+++ assets/js/mail.js
@@ -0,0 +1,77 @@
+
+
+// modul--login
+const modals = document.querySelectorAll('.js-modal')
+const containers = document.querySelectorAll('.js-modal-container')
+const modalLogin = document.querySelector('.js-modal--login')
+const modalSignup = document.querySelector('.js-modal--signup')
+const login = document.querySelector('.js-login')
+const signup = document.querySelector('.js-signup')
+const backLogin = document.querySelector('.js-close-login')
+const backSignpu = document.querySelector('.js-close-signup')
+
+
+// forwark
+const forwarkLogin = document.querySelector('.js-forward-login')
+const forwarkSignup = document.querySelector('.js-forward-signup')
+
+//open--modal
+function Openlogin(){
+    modalLogin.classList.add('open')
+}
+
+function Opensignin(){
+    modalSignup.classList.add('open')
+}
+
+//close--modal
+
+function closeModal(){
+    modalLogin.classList.remove('open')
+    modalSignup.classList.remove('open')
+}
+
+function closeLogin(){
+    modalLogin.classList.remove('open')
+}
+
+function closeSignup(){
+    modalSignup.classList.remove('open')
+}
+
+//click--forwark
+function clickforwarkLogin(){
+    modalSignup.classList.remove('open');
+    modalLogin.classList.add('open')
+}
+
+function clickforwarkSignup(){
+    modalLogin.classList.remove('open');
+    modalSignup.classList.add('open')
+}
+//EvenClick
+login.addEventListener('click',Openlogin)
+signup.addEventListener('click',Opensignin)
+backLogin.addEventListener('click',closeLogin)
+backSignpu.addEventListener('click',closeSignup)
+forwarkLogin.addEventListener('click',clickforwarkLogin)
+forwarkSignup.addEventListener('click',clickforwarkSignup)
+
+//closelayout
+
+for(const modal of modals){
+    modal.addEventListener('click',closeModal)
+}
+
+function notClose(ever){
+    ever.stopPropagation()
+}
+for(const container of containers){
+    container.addEventListener('click',notClose)
+}
+
+
+
+
+
+
